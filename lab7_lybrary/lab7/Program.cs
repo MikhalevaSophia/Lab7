2d50d745bf45b4682ea9b898038ca496cb6dda66
MikhalevaSophia <112760887+MikhalevaSophia@users.noreply.github.com>
2023-10-16 10:17:52 +0300

    Add files via upload

---
+++ lab7_lybrary/lab7/Program.cs
@@ -0,0 +1,49 @@
+﻿using library;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace lab7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly assembly = Assembly.Load(new AssemblyName("animals"));
+            void AppendXML(string output)
+            {
+                File.AppendAllText("AssemblyIfo.xml", output + "\n");
+            }
+            File.Delete("AssemblyInfo.xml");
+            AppendXML("<animals>");
+            AppendXML(assembly.FullName);
+
+            foreach(Type type in assembly.ExportedTypes)
+            {
+                if (type.GetTypeInfo().IsClass)
+                {
+                    AppendXML($"\n<class> {type.Name} : {type.BaseType}");//информация о классе 
+                    IEnumerable<CommentAttribute> attributes = type.GetTypeInfo()
+                                                                   .GetCustomAttribute()
+                                                                   .OfType<CommentAttribute>().ToArray;//считывание комментов
+                    if(attributes.Count() > 0)//вывод комментариев
+                    {
+                        foreach(CommentAttribute attribute in  attributes)
+                        {
+                            AppendXML($"<comment>{attribute.Comment}</comment>");
+                        }
+                    }
+                    foreach(MethodInfo method in type.GetMethods())
+                    {
+                        AppendXML($"<method>{(method.IsStatic ? "static" : "")}{(method.IsVirtual ? "virtual" : "")}{method.ReturnType.Name} {method.Name}()</method>");
+                    }
+                    AppendXML("</class>");
+                }
+            }
+            AppendXML("</amimals>");
+        }
+    }
+}
---
+++ lab7_lybrary/lab7_lybrary/Lybrary.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace library
+{
+    public enum eClassifiationAnimal
+    {
+        Herbovores, Carnivores, Omnivores,
+    }
+    public enum eFavouriteFood { Meet, Plants, Everything,}
+    [Comment("They are animals")]
+    public abstract class Animal
+    {
+        public abstract string Country { get; set;  }
+        public abstract bool HideFromOtherAnimals { get; set;  }
+        public abstract string Name { get; set; }
+        public abstract string WhatAniml { get; set; }
+        public Animal(string country, bool hide, string name, string what)
+        {
+            Country = country;
+            HideFromOtherAnimals = hide;
+            Name = name;
+            WhatAniml = what;
+        }
+        public void Deconstruct() { }
+        public virtual void GetClassificationAnimal()
+        {
+            Console.WriteLine();
+        }
+        public virtual void GetFavoriteFood()
+        {
+            Console.WriteLine();
+        }
+        public virtual void SayHello() {  Console.WriteLine(); }
+    }
+    public abstract class Cow : Animal
+    {
+        public Cow(string county,bool hide, string name) : base (county, hide, name, "Cow") { }
+        public override void GetClassificationAnimal()
+        {
+            Console.WriteLine(eClassifiationAnimal.Herbovores);
+        }
+        public override void GetFavoriteFood()
+        {
+            Console.WriteLine(eFavouriteFood.Plants);
+        }
+        public override void SayHello()
+        {
+            Console.WriteLine("Muuuuuuuu");
+        }
+    }
+    public abstract class Lion : Animal
+    {
+        public Lion(string county, bool hide, string name) : base(county, hide, name, "Lion") { }
+        public override void GetClassificationAnimal()
+        {
+            Console.WriteLine(eClassifiationAnimal.Carnivores);
+        }
+        public override void GetFavoriteFood()
+        {
+            Console.WriteLine(eFavouriteFood.Meet);
+        }
+        public override void SayHello()
+        {
+            Console.WriteLine("Raaaaaar");
+        }
+    }
+    public abstract class Pig : Animal
+    {
+        public Pig(string county, bool hide, string name) : base(county, hide, name, "Pig") { }
+        public override void GetClassificationAnimal()
+        {
+            Console.WriteLine(eClassifiationAnimal.Omnivores);
+        }
+        public override void GetFavoriteFood()
+        {
+            Console.WriteLine(eFavouriteFood.Everything);
+        }
+        public override void SayHello()
+        {
+            Console.WriteLine("Hruuuuuuuuu");
+        }
+    }
+}
